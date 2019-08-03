d69262294de7743891225c7b9f58e5ad015948a0
JakeusMeakus <JM219785@falmouth.ac.uk>
2019-08-03 18:10:10 +0100

    TitleScreen finished for now

---
+++ One Strike/Assets/Scripts/StarsScroll.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsScroll : MonoBehaviour
+{
+    float scrollSpeed = 0.01f;
+
+
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        transform.position -= new Vector3(scrollSpeed * Time.deltaTime, 0, 0);
+    }
+    
+}

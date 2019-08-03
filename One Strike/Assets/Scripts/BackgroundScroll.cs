de889e53b4be5fa2b15dc3b931ad87d614df6843
JakeusMeakus <JM219785@falmouth.ac.uk>
2019-08-03 04:20:02 +0100

    Backup Boi

---
+++ One Strike/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    [SerializeField] float xSpeed = 1f;
+    [SerializeField] float ySpeed = 1f;
+
+    Vector2 pos = new Vector2(0.2f, 0.2f);
+    Vector2 offset = Vector2.zero;
+    Material mat;
+
+    private void Awake()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+    }
+
+    private void FixedUpdate()
+    {
+        Scroll(pos);
+    }
+
+    public void Scroll(Vector2 pos)
+    {
+        offset.x += pos.x * xSpeed;
+        offset.y += pos.y * ySpeed;
+
+        if(offset.x > 1f)
+        {
+            offset.x -= 1f;
+        }
+        else if(offset.x < 1f)
+        {
+            offset.x += 1f;
+        }
+
+        if(offset.y > 1f)
+        {
+            offset.y -= 1f;
+        }
+        else if(offset.y < 1f)
+        {
+            offset.y += 1f;
+        }
+
+        mat.mainTextureOffset = offset;
+    }
+}
---
+++ One Strike/Assets/Scripts/GameManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    float timer;
+    AudioSource audioSource;
+    AudioClip otherClip;
+
+    [SerializeField] AudioClip[] clips;
+    [SerializeField] GameObject strikeCanvas;
+    [SerializeField] GameObject winCanvas;
+    [SerializeField] GameObject loseCanvas;
+    [SerializeField] Text countdownText;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        strikeCanvas.SetActive(false);
+        timer = Random.Range(10, 30);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.PlayOneShot(clips[0]);
+        Countdown();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdownText.text = timer.ToString();
+        timer -= Time.deltaTime;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Restart()
+    {
+        audioSource.PlayOneShot(clips[4]);
+        SceneManager.LoadSceneAsync(1);
+    }
+    
+    public void MainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    void Countdown()
+    {         
+        Invoke("Strike", timer);
+    }
+
+    void Strike()
+    {
+        strikeCanvas.SetActive(true);
+        StartCoroutine(CheckPress());
+        audioSource.Stop();
+        audioSource.PlayOneShot(clips[1]);
+        audioSource.PlayOneShot(clips[2]);
+        otherClip = clips[3];
+        audioSource.Play();
+        audioSource.loop = true;
+    }
+
+    IEnumerator CheckPress()
+    {
+        float timer = 0.4f;
+        bool success = false;
+        while(success == false && timer > 0f)
+        {
+            timer -= Time.deltaTime;
+            success = Input.GetKeyDown(KeyCode.Space);
+            yield return null;
+        }
+        if(success == false)
+        {
+            Debug.Log("Failed");
+            strikeCanvas.SetActive(false);
+            loseCanvas.SetActive(true);
+            yield break;
+        }
+        Debug.Log("Won");
+        strikeCanvas.SetActive(false);
+        winCanvas.SetActive(true);
+    }
+
+   
+}
